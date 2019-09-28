9125082d3d6c4e542051ee6ebe7ac3e49950b817
baltz <baltazar.parra@fliper.app>
2019-09-27 23:12:22 -0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  return <div className="App">Hell Low</div>
+  const [repositories, setRepositories] = useState([])
+  const [location, setLocation] = useState([])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/baltazarparra/repos')
+    const data = await response.json()
+
+    setRepositories(data)
+  }, [])
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `Você tem ${filtered.length} favoritos`
+  }, [repositories])
+
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived)
+    return () => navigator.geolocation.clearWatch(watchId)
+  }, [])
+
+  function handleFavorite (id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords
+    setLocation({ latitude, longitude })
+  }
+
+  return (
+    <>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            <button onClick={() => handleFavorite(repo.id)}>
+              Fav
+            </button>
+            {repo.favorite && <span> - (S2) - </span>}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
